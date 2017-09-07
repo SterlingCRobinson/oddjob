31bddecc3574613f7289493ebd918580e8acd38a
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-09-07 17:40:39 -0400

    a

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :users {
 		get 'users/:id', to: 'users#show', as:'user'
 	}
+	root to: "home#index"
 	resources :jobs, :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
