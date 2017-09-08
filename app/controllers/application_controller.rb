9e5ca97c713ce33a529c49fc3fb6a95b4f0dfcda
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-09-08 18:44:24 -0400

    add profile components desc not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+ 	def index
+		@user = User.find_by(params[:id])
+	end
+
+	private
+
+	def user_params
+	    params.require(:user).permit(:fname, :lname, :email, :avatar, :id)
+	end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
 	def index
-		
+		@user = User.find_by(params[:id])
+	end
+
+	private
+
+	def user_params
+	    params.require(:user).permit(:fname, :lname, :email, :description, :avatar, :id)
 	end
 end
