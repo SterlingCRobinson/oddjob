b26768598a89eea660acb2925e3bf547f5778d1c
Jeremy Leitmeyer <jeremyleitmeyer@gmail.com>
2017-09-13 14:42:32 -0400

    fix button on job show

--- public/js/jobs.js
+++ public/js/jobs.js
@@ -1,7 +1,8 @@
-var btn = document.querySelector(".accept-btn")
+var btn = document.querySelector("#accept")
 
 btn.addEventListener("click", function(){
 	alert("Your info has been sent to the Employer")
 	btn.disabled = true;
 	btn.innerHTML = "Accepted"
+	btn.classList.remove("submitty")
 })
